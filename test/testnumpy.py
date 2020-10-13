b15de70ff5cb5bdf95aa915e0007072aa204c14e
tuxedoking <387889277@qq.com>
2020-10-13 17:39:46 +0800

    aaaa

--- test/testnumpy.py
+++ test/testnumpy.py
@@ -1,6 +1,6 @@
 import numpy as np
 a = np.arange(10, 22).reshape(3, 4)
 print(a)
-array = np.array([[1, 2, 3, 4, 5], [2, 3, 4, 5, 6]])
+array = np.array([[1, 2, 3, 4, 5], [2, 3, 4, 5, 7]])
 print(type(array))
 print(array.dtype)
