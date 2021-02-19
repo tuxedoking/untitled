3316326ffa1cdc24027ae3b65ac73a651e49ba6e
tuxedoking <387889277@qq.com>
2021-02-19 16:23:12 +0800

    aa

--- checkdayline_dbm.py
+++ checkdayline_dbm.py
@@ -5,7 +5,7 @@ import os
 if __name__ == '__main__':
     try:
         # db = dbm.open(os.getcwd() + '/dbms/day_line.dbm')
-        #db = dbm.open(os.getcwd() + '/dbms/week_line.dbm')
+        # db = dbm.open(os.getcwd() + '/dbms/week_line.dbm')
         db = dbm.open(os.getcwd() + '/dbms/month_line.dbm')
         for key in db.keys():
             data = db[key]
@@ -16,15 +16,13 @@ if __name__ == '__main__':
                 break
         db.close()
 
-        db = dbm.open(os.getcwd() + '/dbms/month_line.dbm')
-        for key in db.keys():
-            data = db[key]
-            lines = pickle.loads(data)
+        db_bo_lang_s = dbm.open(os.getcwd() + '/dbms/bo_lang_s.dbm')
+        for key in db_bo_lang_s.keys():
+            data = db_bo_lang_s[key]
+            bo_lang_s = pickle.loads(data)
             code = bytes.decode(key)
-            for date in sorted(lines, reverse=True):
-                print(code, date)
-                break
-        db.close()
+            print(code, bo_lang_s)
+        db_bo_lang_s.close()
     except Exception as err:
         print(err)
     finally:
