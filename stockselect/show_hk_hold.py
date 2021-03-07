e70b86043d531cd9ae5ad5dff55a5eba1ec48a03
tuxedoking <387889277@qq.com>
2021-03-07 15:06:46 +0800

    tuxedo

--- stockselect/show_hk_hold.py
+++ stockselect/show_hk_hold.py
@@ -8,9 +8,9 @@ import json
 if __name__ == '__main__':
     try:
         # codes = {'002531.SZ', '300418.SZ'}
-        code_table = codetable.read_codetable(os.getcwd() + '/dbms/codetable.dbm')
-        db = dbm.open(os.getcwd() + '/dbms/hk_hold.dbm')
-        with open(os.getcwd() + '/select_result/hk_hold.txt', 'w') as f:
+        code_table = codetable.read_codetable(os.getcwd() + '/../dbms/codetable.dbm')
+        db = dbm.open(os.getcwd() + '/../dbms/hk_hold.dbm')
+        with open(os.getcwd() + '/../select_result/hk_hold.txt', 'w') as f:
             for key in db.keys():
                 data = db[key]
                 hold = pickle.loads(data)
