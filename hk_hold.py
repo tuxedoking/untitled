429c643ad10455b7de16baefe3d622c2f2fa413f
tuxedoking <387889277@qq.com>
2021-03-16 17:22:37 +0800

    tuxedo

--- hk_hold.py
+++ hk_hold.py
@@ -35,7 +35,7 @@ def get_hk_hold(trade_date):
 
 
 if __name__ == '__main__':
-    get_hk_hold(20210303)
+    get_hk_hold(20210315)
     # ds = ds_ts.Datasource()
     # day_set = ds.get_trade_days()
     # day_list = sorted(day_set, reverse=True)[0:100]
