c846181e0ade2a8f04ae848a5839314440b2885e
tuxedoking <387889277@qq.com>
2021-04-22 18:52:46 +0800

    tuxedo

--- stockselect/zhang_ting.py
+++ stockselect/zhang_ting.py
@@ -1,55 +1,56 @@
-import dbm
 import pickle
-import sys
-import codetable
-import os
 from util import is_zt
-from util import get_start_date
-from util import __end_date
 from util import get_stock_name
-import csv
+from stockselect.selector import selector
 
 
-def select():
-    try:
-        start_date = get_start_date()
-        with open(os.getcwd() + '/../select_result/涨停.csv', 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            csv_writer.writerow(['日期', '代码', '名称', '数量'])
-            csv_writer.writerow(['80', '60', '80', '60'])
-            csv_writer.writerow(['int', 'string', 'string', 'int'])
+class zhang_ting(selector):
+    def __init__(self):
+        super().__init__()
 
-            db = dbm.open(os.getcwd() + '/../dbms/day_line.dbm')
-            for key in db.keys():
-                data = db[key]
-                day_lines = pickle.loads(data)
+    def select(self, **kwargs):
+        try:
+            if 'start_date' not in kwargs:
+                return None
+            if 'max_day_count' not in kwargs:
+                max_day_count = 50
+            else:
+                max_day_count = kwargs['max_day_count']
+            start_date = kwargs['start_date']
+            items = []
+            cache_key = __file__ + start_date + str(max_day_count)
+
+            if cache_key in selector.result_cache:
+                return selector.result_cache[cache_key]
+
+            for key in self.db_day_lines.keys():
+                data = self.db_day_lines[key]
+                df_lines = pickle.loads(data)
                 code = bytes.decode(key)
                 count = 0
-                for date in sorted(day_lines, reverse=True):
-                    if int(date) > __end_date:
-                        continue
-                    elif start_date <= int(date) <= __end_date:
-                        pre_close = day_lines[date]['raw'][4]
-                        close = day_lines[date]['raw'][3]
-                        b = is_zt(pre_close, close)
-                        if b is True:
-                            if count == 0:
-                                to_date = date
-                            count += 1
-                        else:
-                            if count > 0:
-                                result_row = [to_date, code[0:6], get_stock_name(code), count]
-                                csv_writer.writerow(result_row)
-                                print(result_row)
+                to_date = ''
+                for index in df_lines.index:
+                    pre_close = df_lines.loc(index, 'pre_close')
+                    close = df_lines.loc(index, 'close')
+                    b = is_zt(pre_close, close)
+                    if b is True:
+                        # item = (df_lines.loc(index, 'trade_date'), code, get_stock_name(code), count)
+                        if count == 0:
+                            to_date = df_lines.loc(index, 'trade_date')
+                        count += 1
+                    else:
+                        if count > 0:
+                            item = (to_date, code, get_stock_name(code), count)
+                            items.append(item)
+                            print(item)
                             count = 0
-                    elif int(date) < start_date:
-                        break
-            db.close()
-    except Exception as err:
-        print(err)
-    finally:
-        pass
+            selector.result_cache[cache_key] = items
+            return items
+        except Exception as err:
+            print(err)
+            return None
 
 
 if __name__ == '__main__':
-    select()
+    a = zhang_ting()
+    a.select(start_date='20210101')
